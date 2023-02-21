1501761b3f527b34a8652094b6b8136c7b8f9f82
grant0711 <adamsgrant87@gmail.com>
2023-02-21 20:40:31 +0000

    Add another junk function to test coverage report on CI

--- api/events/views.py
+++ api/events/views.py
@@ -8,3 +8,14 @@ def here_is_a_dummy_function():
             print('yes!')
 
     return True
+
+def here_is_another():
+    for i in range(100):
+        k = i / 2
+
+    try:
+        i = 5
+    except Exception:
+        pass
+
+    return False
