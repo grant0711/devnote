72b1df8561cb005e80155e14e03dba622f3b6580
grant0711 <adamsgrant87@gmail.com>
2023-02-21 18:36:44 +0000

    Refactor devnote to class

--- app/devnote.py
+++ app/devnote.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 import subprocess
 import datetime
 from pathlib import Path
@@ -8,46 +9,65 @@ import urllib
 from urllib import request, parse
 
 
+class DevNote():
+    def __init__(self, now: datetime.datetime=None):
+        if now == None:
+            now = datetime.datetime.now(datetime.timezone.utc)
+        self.date = now.strftime('%Y-%m-%d')
+        self.filepath = self.set_filepath()
+        self.create()
+
+    def set_filepath(self):
+        return f"{os.environ['DEVNOTE_LOCAL']}/{self.date}.txt"
+
+    def title(self):
+        return f"# Notes for {self.date}:\n"
+        
+    def create(self):
+        if not Path(self.filepath).is_file():
+            subprocess.call(["touch", self.filepath])
+            self.append(self.title(), with_update=False)
+
+    def append(self, text: str, with_update: bool=True):
+        with open(self.filepath, 'a') as file:
+            file.write(text)
+            file.write('\n')
+        if with_update:
+            self.post()
+
+    def interactive_edit(self):
+        start_hash = self.hash()
+        subprocess.call(["nano", self.filepath])
+        end_hash = self.hash()
+        if start_hash != end_hash:
+            self.post()
+
+    def readfile(self):
+        with open(self.filepath, 'r') as file:
+            return file.read()
+
+    def hash(self):
+        content = self.readfile()
+        return md5(content.encode()).hexdigest
+
+    def post(self):
+        data = parse.urlencode({
+            'content': self.readfile(),
+            'date': self.date
+        }).encode()
+        req =  request.Request(os.environ['DEVNOTE_URL'], data=data)
+        req.add_header('Authorization', f"Token {os.environ['DEVNOTE_TOKEN']}")
+        req.add_header('Content-Type', 'application/json')
+        try:
+            with request.urlopen(req, timeout=30) as response:
+                print(response)
+        except urllib.error.HTTPError as e:
+            print(f'failed to post devnote: {e}')
+
 
 if __name__ == "__main__":
-    # Use the YYYYMMDD as the filename
-    date = datetime.datetime.now().strftime('%Y%m%d')
-
-    filepath = f"{os.environ['DEVNOTE_LOCAL']}/{date}.txt"
-    if not Path(filepath).is_file():
-        # Create our file if it doesn't exist
-        subprocess.call(["touch", filepath])
-        start_hash = ''
+    devnote = DevNote()
+    if len(sys.argv) > 1:
+        devnote.append(' '.join(sys.argv[1:]))
     else:
-        # Read our md5 hash of the file
-        with open(filepath, 'r') as infile:
-            content = infile.read()
-            start_hash = md5(content.encode()).hexdigest()
-    
-    # Open up nano to start editing file
-    subprocess.call(["nano", filepath])
-
-    # After closing nano read in our file here
-    with open(filepath, 'r') as infile:
-        content = infile.read()
-
-    # Check our hash to see if we changed the file
-    if start_hash == md5(content.encode()).hexdigest():
-        print(f'devnote {date}.txt not modified')
-        exit()
-
-    # Post our file content to our endpoint
-    data = parse.urlencode({
-        'content': content,
-        'date': date
-    }).encode()
-    req =  request.Request(os.environ['DEVNOTE_URL'], data=data)
-    req.add_header('Authorization', f"Token {os.environ['DEVNOTE_TOKEN']}")
-    req.add_header('Content-Type', 'application/json')
-    try:
-        with request.urlopen(req, timeout=30) as response:
-            print(response)
-    except urllib.error.HTTPError as e:
-        print(f'failed to post devnote: {e}')
-
-    
+        devnote.interactive_edit()
