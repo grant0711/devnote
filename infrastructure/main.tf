b07dc953c88e38e188db974c8eb70afb93f03bd9
grant0711 <adamsgrant87@gmail.com>
2023-03-03 06:44:37 +0000

    Add postgres; secret key and web formation to terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,14 +20,35 @@ resource "heroku_app" "app" {
   ]
 }
 
+resource "random_password" "key" {
+  length   = 32
+  special  = false
+}
+
 resource "heroku_config" "common" {
   vars = {
     DJANGO_SETTINGS_MODULE = "api.settings.prod"
     APP_BASE               = "api"
   }
+  sensitive_vars = {
+    SECRET_KEY             = random_password.key.result
+  }
 }
 
 resource "heroku_app_config_association" "config-association" {
-  app_id = heroku_app.app.id
-  vars           = heroku_config.common.vars
+  app_id   = heroku_app.app.id
+  vars     = heroku_config.common.vars
+}
+
+resource "heroku_formation" "web-formation" {
+  count    = var.web-dynos > 0 ? 1 : 0
+  app_id   = heroku_app.app.id
+  type     = "web"
+  quantity = var.web-dynos
+  size     = "eco"
+}
+
+resource "heroku_addon" "database" {
+  app_id  = heroku_app.app.id
+  plan    = "heroku-postgresql:mini"
 }
